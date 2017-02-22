843f868563d0b706934428600d9116af291431f1
rhysjparry <rhys.parry@basekit.com>
2017-02-22 16:27:19 +0000

    Updates.

---
+++ misc/demo-ui.js
@@ -0,0 +1,106 @@
+
+// Row Colour Switchers
+$('.row__hero--01').click( function(){
+  $('.row__hero--01').toggleClasses(['light', 'accent', 'dark']);
+});
+
+$('.row__hero--02').click( function(){
+  $('.row__hero--02').toggleClasses(['light', 'accent', 'dark']);
+});
+
+$('.row__hero--03').click( function(){
+  $('.row__hero--03').toggleClasses(['light', 'accent', 'dark']);
+});
+
+$('.row__content--02').click( function(){
+  $('.row__content--02').toggleClasses(['light', 'accent', 'dark']);
+});
+
+$('.row__gallery--01').click( function(){
+  $('.row__gallery--01').toggleClasses(['light', 'accent', 'dark']);
+});
+
+$('.row__gallery--02').click( function(){
+  $('.row__gallery--02').toggleClasses(['light', 'accent', 'dark']);
+});
+
+$('.row__gallery--03').click( function(){
+  $('.row__gallery--03').toggleClasses(['light', 'accent', 'dark']);
+});
+
+$('.row__content--03').click( function(){
+  $('.row__content--03').toggleClasses(['light', 'accent', 'dark']);
+});
+
+$('.row__contact--02').click( function(){
+  $('.row__contact--02').toggleClasses(['light', 'accent', 'dark']);
+});
+
+$('.row__contact--03').click( function(){
+  $('.row__contact--03').toggleClasses(['light', 'accent', 'dark']);
+});
+
+$('.row__footer--01').click( function(){
+  $('.row__footer--01').toggleClasses(['light', 'accent', 'dark']);
+});
+
+
+// Hero Layout Switcher
+$(document).ready(function(){
+
+    // Hides all but the first row
+    $('.row + .row').hide();
+
+
+    // Shows the selected row
+    $('.layout-choice--hero').on('change', function() {
+        var val = $(this).val();
+        var row = $('.row__hero--01, .row__hero--02, .row__hero--03');
+
+        row.hide(); // hide all rows
+
+        $('.' + val).show(); // show the current row
+
+    })
+});
+
+
+// Content Layout Switcher
+$(document).ready(function(){
+
+    // Hides all but the first row
+    $('.row + .row').hide();
+
+
+    // Shows the selected row
+    $('.layout-choice--content').on('change', function() {
+        var val = $(this).val();
+        var row = $('.row__content--01, .row__content--02, .row__content--03');
+
+        row.hide(); // hide all rows
+
+        $('.' + val).show(); // show the current row
+
+    })
+});
+
+
+// Content Layout Switcher
+$(document).ready(function(){
+
+    // Hides all but the first row
+    $('.row + .row').hide();
+
+
+    // Shows the selected row
+    $('.layout-choice--gallery').on('change', function() {
+        var val = $(this).val();
+        var row = $('.row__gallery--01, .row__gallery--02, .row__gallery--03');
+
+        row.hide(); // hide all rows
+
+        $('.' + val).show(); // show the current row
+
+    })
+});
+

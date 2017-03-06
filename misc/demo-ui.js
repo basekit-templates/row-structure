9e3239212f53113f4f531e8961beeaa986db277e
Dominic Hickey <dominic.j.hickey@gmail.com>
2017-03-06 11:41:00 +0000

    Updated grid / class names for columns structure, various updates

--- misc/demo-ui.js
+++ misc/demo-ui.js
@@ -1,169 +1,169 @@
 
-// Row Colour Switchers
-$('.row__hero--01').click( function(){
-  $('.row__hero--01').toggleClasses(['light', 'accent', 'dark']);
-});
+// // Row Colour Switchers
+// $('.row__hero--01').click( function(){
+//   $('.row__hero--01').toggleClasses(['light', 'accent', 'dark']);
+// });
 
-$('.row__hero--02').click( function(){
-  $('.row__hero--02').toggleClasses(['light', 'accent', 'dark']);
-});
+// $('.row__hero--02').click( function(){
+//   $('.row__hero--02').toggleClasses(['light', 'accent', 'dark']);
+// });
 
-$('.row__hero--03').click( function(){
-  $('.row__hero--03').toggleClasses(['light', 'accent', 'dark']);
-});
-
-
-// Content Colour Switcher
-$('.row__content--01').click( function(){
-  $('.row__content--01').toggleClasses(['light', 'accent', 'dark']);
-});
-
-$('.row__content--02').click( function(){
-  $('.row__content--02').toggleClasses(['light', 'accent', 'dark']);
-});
-
-$('.row__content--03').click( function(){
-  $('.row__content--03').toggleClasses(['light', 'accent', 'dark']);
-});
+// $('.row__hero--03').click( function(){
+//   $('.row__hero--03').toggleClasses(['light', 'accent', 'dark']);
+// });
 
 
-// Gallery Colour Switcher
-$('.row__gallery--01').click( function(){
-  $('.row__gallery--01').toggleClasses(['light', 'accent', 'dark']);
-});
+// // Content Colour Switcher
+// $('.row__content--01').click( function(){
+//   $('.row__content--01').toggleClasses(['light', 'accent', 'dark']);
+// });
 
-$('.row__gallery--02').click( function(){
-  $('.row__gallery--02').toggleClasses(['light', 'accent', 'dark']);
-});
+// $('.row__content--02').click( function(){
+//   $('.row__content--02').toggleClasses(['light', 'accent', 'dark']);
+// });
 
-$('.row__gallery--03').click( function(){
-  $('.row__gallery--03').toggleClasses(['light', 'accent', 'dark']);
-});
-
-
-// Content Colour Switcher
-$('.row__contact--01').click( function(){
-  $('.row__contact--01').toggleClasses(['light', 'accent', 'dark']);
-});
-
-$('.row__contact--02').click( function(){
-  $('.row__contact--02').toggleClasses(['light', 'accent', 'dark']);
-});
-
-$('.row__contact--03').click( function(){
-  $('.row__contact--03').toggleClasses(['light', 'accent', 'dark']);
-});
+// $('.row__content--03').click( function(){
+//   $('.row__content--03').toggleClasses(['light', 'accent', 'dark']);
+// });
 
 
-// Footer Colour Switcher
-$('.row__footer--01').click( function(){
-  $('.row__footer--01').toggleClasses(['light', 'accent', 'dark']);
-});
+// // Gallery Colour Switcher
+// $('.row__gallery--01').click( function(){
+//   $('.row__gallery--01').toggleClasses(['light', 'accent', 'dark']);
+// });
 
-$('.row__footer--02').click( function(){
-  $('.row__footer--02').toggleClasses(['light', 'accent', 'dark']);
-});
+// $('.row__gallery--02').click( function(){
+//   $('.row__gallery--02').toggleClasses(['light', 'accent', 'dark']);
+// });
 
-$('.row__footer--03').click( function(){
-  $('.row__footer--03').toggleClasses(['light', 'accent', 'dark']);
-});
-
-
-// // Hero Layout Switcher
-$(document).ready(function(){
-
-    // Hides all but the first row
-    $('.row + .row').hide();
+// $('.row__gallery--03').click( function(){
+//   $('.row__gallery--03').toggleClasses(['light', 'accent', 'dark']);
+// });
 
 
-    // Shows the selected row
-    $('.layout-choice--hero').on('change', function() {
-        var val = $(this).val();
-        var row = $('.row__hero--01, .row__hero--02, .row__hero--03');
+// // Content Colour Switcher
+// $('.row__contact--01').click( function(){
+//   $('.row__contact--01').toggleClasses(['light', 'accent', 'dark']);
+// });
 
-        row.hide(); // hide all rows
+// $('.row__contact--02').click( function(){
+//   $('.row__contact--02').toggleClasses(['light', 'accent', 'dark']);
+// });
 
-        $('.' + val).show(); // show the current row
-
-    })
-});
-
-
-// Content Layout Switcher
-$(document).ready(function(){
-
-    // Hides all but the first row
-    $('.row + .row').hide();
+// $('.row__contact--03').click( function(){
+//   $('.row__contact--03').toggleClasses(['light', 'accent', 'dark']);
+// });
 
 
-    // Shows the selected row
-    $('.layout-choice--content').on('change', function() {
-        var val = $(this).val();
-        var row = $('.row__content--01, .row__content--02, .row__content--03');
+// // Footer Colour Switcher
+// $('.row__footer--01').click( function(){
+//   $('.row__footer--01').toggleClasses(['light', 'accent', 'dark']);
+// });
 
-        row.hide(); // hide all rows
+// $('.row__footer--02').click( function(){
+//   $('.row__footer--02').toggleClasses(['light', 'accent', 'dark']);
+// });
 
-        $('.' + val).show(); // show the current row
-
-    })
-});
-
-
-// Content Layout Switcher
-$(document).ready(function(){
-
-    // Hides all but the first row
-    $('.row + .row').hide();
+// $('.row__footer--03').click( function(){
+//   $('.row__footer--03').toggleClasses(['light', 'accent', 'dark']);
+// });
 
 
-    // Shows the selected row
-    $('.layout-choice--gallery').on('change', function() {
-        var val = $(this).val();
-        var row = $('.row__gallery--01, .row__gallery--02, .row__gallery--03');
+// // // Hero Layout Switcher
+// $(document).ready(function(){
 
-        row.hide(); // hide all rows
-
-        $('.' + val).show(); // show the current row
-
-    })
-});
+//     // Hides all but the first row
+//     $('.row + .row').hide();
 
 
-// Contact Layout Switcher
-$(document).ready(function(){
+//     // Shows the selected row
+//     $('.layout-choice--hero').on('change', function() {
+//         var val = $(this).val();
+//         var row = $('.row__hero--01, .row__hero--02, .row__hero--03');
 
-    // Hides all but the first row
-    $('.row + .row').hide();
+//         row.hide(); // hide all rows
 
+//         $('.' + val).show(); // show the current row
 
-    // Shows the selected row
-    $('.layout-choice--contact').on('change', function() {
-        var val = $(this).val();
-        var row = $('.row__contact--01, .row__contact--02, .row__contact--03');
-
-        row.hide(); // hide all rows
-
-        $('.' + val).show(); // show the current row
-
-    })
-});
+//     })
+// });
 
 
-// Contact Layout Switcher
-$(document).ready(function(){
+// // Content Layout Switcher
+// $(document).ready(function(){
 
-    // Hides all but the first row
-    $('.row + .row').hide();
+//     // Hides all but the first row
+//     $('.row + .row').hide();
 
 
-    // Shows the selected row
-    $('.layout-choice--footer').on('change', function() {
-        var val = $(this).val();
-        var row = $('.row__footer--01, .row__footer--02, .row__footer--03');
+//     // Shows the selected row
+//     $('.layout-choice--content').on('change', function() {
+//         var val = $(this).val();
+//         var row = $('.row__content--01, .row__content--02, .row__content--03');
 
-        row.hide(); // hide all rows
+//         row.hide(); // hide all rows
 
-        $('.' + val).show(); // show the current row
+//         $('.' + val).show(); // show the current row
 
-    })
-});
+//     })
+// });
+
+
+// // Content Layout Switcher
+// $(document).ready(function(){
+
+//     // Hides all but the first row
+//     $('.row + .row').hide();
+
+
+//     // Shows the selected row
+//     $('.layout-choice--gallery').on('change', function() {
+//         var val = $(this).val();
+//         var row = $('.row__gallery--01, .row__gallery--02, .row__gallery--03');
+
+//         row.hide(); // hide all rows
+
+//         $('.' + val).show(); // show the current row
+
+//     })
+// });
+
+
+// // Contact Layout Switcher
+// $(document).ready(function(){
+
+//     // Hides all but the first row
+//     $('.row + .row').hide();
+
+
+//     // Shows the selected row
+//     $('.layout-choice--contact').on('change', function() {
+//         var val = $(this).val();
+//         var row = $('.row__contact--01, .row__contact--02, .row__contact--03');
+
+//         row.hide(); // hide all rows
+
+//         $('.' + val).show(); // show the current row
+
+//     })
+// });
+
+
+// // Contact Layout Switcher
+// $(document).ready(function(){
+
+//     // Hides all but the first row
+//     $('.row + .row').hide();
+
+
+//     // Shows the selected row
+//     $('.layout-choice--footer').on('change', function() {
+//         var val = $(this).val();
+//         var row = $('.row__footer--01, .row__footer--02, .row__footer--03');
+
+//         row.hide(); // hide all rows
+
+//         $('.' + val).show(); // show the current row
+
+//     })
+// });
